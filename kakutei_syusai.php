03e4ca00f02b2b28e1b712f0c0b2a5a941fb9084
fujishiro <ksw2070168@stu.o-hara.ac.jp>
2021-11-09 15:51:18 +0900

    11/09

---
+++ kakutei_syusai.php
@@ -0,0 +1,24 @@
+<?php require_once("header.php"); ?>
+  <!-- Header
+        <header class="bg-dark py-5">
+            <div class="container px-4 px-lg-5 my-5">
+                <div class="text-center text-white">
+                    <h1 class="display-4 fw-bolder">Shop in style</h1>
+                    <p class="lead fw-normal text-white-50 mb-0">With this shop hompeage template</p>
+                </div>
+            </div>
+        </header>
+        -->
+  <!-- Section-->
+  <section class="container py-5">
+    <div class="">
+    <form method="$_POST" action="top.php">
+    <p> 月 日で人数は 人で のイベントを開催します。<br>
+    抽選方法は です。
+    </p>
+    
+  <p><input class="btn btn-success" type="submit" value="OK"></p>  
+  </form>
+    </div>
+  </section>
+  <?php require_once("footer.php"); ?>
---
+++ login_syusai.php
@@ -0,0 +1,24 @@
+<?php require_once("header.php"); ?>
+  <!-- Header
+        <header class="bg-dark py-5">
+            <div class="container px-4 px-lg-5 my-5">
+                <div class="text-center text-white">
+                    <h1 class="display-4 fw-bolder">Shop in style</h1>
+                    <p class="lead fw-normal text-white-50 mb-0">With this shop hompeage template</p>
+                </div>
+            </div>
+        </header>
+        -->
+  <!-- Section-->
+  <section class="container py-5">
+    <div class="">
+    <form method="$_POST" action="select_syusai.php">
+      <h2>会員確認</h2>
+    <p>e-mail：<input class="form-control" type="text"></p>
+    <p>パスワードを入力してください: <input class="form-control" type="text"></p>
+    <p><input class="btn btn-success" type="submit" value="OK"></p>
+  </form>
+    </div>
+  </section>
+  <!-- Footer-->
+  <?php require_once("footer.php"); ?>
