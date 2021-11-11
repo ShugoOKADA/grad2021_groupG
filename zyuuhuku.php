b74d3b8083ab79df5c79815078b35c97c423ef1b
fujishiro <ksw2070168@stu.o-hara.ac.jp>
2021-11-11 10:27:18 +0900

    11/11

---
+++ zyuuhuku.php
@@ -0,0 +1,23 @@
+<?php require_once("header.php"); ?>
+<!-- Header
+    
+        <header class="bg-dark py-5">
+            <div class="container px-4 px-lg-5 my-5">
+                <div class="text-center text-white">
+                    <h1 class="display-4 fw-bolder">Shop in style</h1>
+                    <p class="lead fw-normal text-white-50 mb-0">With this shop hompeage template</p>
+                </div>
+            </div>
+        </header>
+        -->
+<!-- Section-->
+<section class="container py-5">
+  <div class="">
+    <form method="$_POST" action="event.php">
+      <p>もうすでに予約されているため予約することはできません。</p>
+      <p><input class="btn btn-success" type="submit" value="OK"></p>
+    </form>
+  </div>
+</section>
+
+<?php require_once("footer.php"); ?>
