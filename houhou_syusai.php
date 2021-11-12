ae6fb2868f5ac684545c88bc91a2562115a599f3
Okada Shugo <ksw2070101@stu.o-hara.ac.jp>
2021-11-12 15:07:55 +0900

    bootstrap変更

--- houhou_syusai.php
+++ houhou_syusai.php
@@ -4,7 +4,7 @@
     <h2>予約方法をお選び下さい</h2>
     <input type="radio" name="gender" value="male">早い者勝ち
 　　 <input type="radio" name="gender" value="female">抽選<br><br>
-    <input type="button" onclick="location.href='./houhou_syusai.php'" value="確定">
+    <input type="button" onclick="location.href='./kakunin_syusai.php'" value="確定">
         </section>
         <!-- Footer-->
 <?php require_once ("footer.php"); ?>
