d4eec3bc162e5585fa7901b0584028fefffce217
fujishiro <ksw2070168@stu.o-hara.ac.jp>
2021-11-16 09:50:22 +0900

    修正

--- touroku.php
+++ touroku.php
@@ -1,12 +1,11 @@
-
-<?php require_once ("header.php"); ?>
-        <section class="py-5">
-        <form action="hanabi.php" method="post">
+<?php require_once("header.php"); ?>
+<section class="py-5">
+    <form action="touroku_check.php" method="post">
         <p>名前を入力してください：<input class="form-control" type="text" name="user"></p>
         <p>email：<input class="form-control" type="text" name="address"></p>
         <p>パスワード：<input class="form-control" type="password" name="password"></p>
         <p>確認のためもう1度パスワードを入力してください：<input class="form-control" type="password" name="password"></p>
-        <button type="submit" class="btn btn-success"><a href="pass_error.php">OK</a></button>
-        </section>
-<?php require_once ("footer.php"); ?>
-
+        <input type="submit" class="btn btn-success" value="OK">
+    </form>
+</section>
+<?php require_once("footer.php"); ?>
