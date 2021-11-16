ee46c64475356b4330e6e3631d93e4c9e4447498
Okada Shugo <ksw2070101@stu.o-hara.ac.jp>
2021-11-16 09:45:22 +0900

    重複機能実装

---
+++ zyuhuku.php
@@ -0,0 +1,10 @@
+<?php require_once("header.php");?>
+<!-- Section-->
+        <section class="py-5">
+        <p>もうすでに予約されているため予約することはできません。</p>
+
+    <input type="button" class="btn btn-success" onclick="location.href='./top.php'" value="OK">
+    
+        </section>
+       
+     <?php require_once("footer.php");?>
