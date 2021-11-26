74218830261ad7bf17712569b804b3b60839925a
ruka0315 <ksw207032@stu.o-hara.ac.jp>
2021-11-26 16:21:48 +0900

    チェックとエラーの修正

--- reserve/kakutei_hayaimono.php
+++ reserve/kakutei_hayaimono.php
@@ -1,9 +1,9 @@
 <?php require_once ("../header.php"); ?>
         <section class="py-5">
         <form method="post" action="../top.php">
-        <h1>予約完了</h1>
+        <h2>予約完了</h2>
                     <p>予約が完了しました。以下の予約番号を控えおいてください。</p>
-                    <h3>予約情報</h3>
+                    <h2>予約情報</h2>
                     <table class="input">
                         <tr><th>予約番号</th><td>xxxx</td></tr>
                     </table>
