8649cd8249ef93819641baf5658e10854c74545b
okada <shugo140208@softbank.ne.jp>
2021-11-30 10:07:04 +0900

    リンク修正

--- reserve/kakutei_hayaimono.php
+++ reserve/kakutei_hayaimono.php
@@ -1,6 +1,6 @@
 <?php require_once ("../header.php"); ?>
         <section class="py-5">
-        <form method="post" action="../top.php">
+        <form method="post" action="../top_kyaku.php">
         <h1>予約完了</h1>
                     <p>予約が完了しました。以下の予約番号を控えおいてください。</p>
                     <h3>予約情報</h3>
