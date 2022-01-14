32cf7926faa2d6ad7c850e6b3ded05d033305dfb
ruka0315 <ksw207032@stu.o-hara.ac.jp>
2022-01-14 15:06:46 +0900

    select修正

--- event/kakunin_syusai.php
+++ event/kakunin_syusai.php
@@ -1,12 +1,13 @@
 <?php require_once("../header.php"); ?>
 <section class="py-5">
         <form action="event_tsuika.php" method="post">
-                <p>開催するイベントは<?= $_POST['name'] ?><br>日付は<?= $_POST['date'] ?><br>スペースは<?= $_POST['size'] ?><br>
-                        予約方法は<br><?= $_POST['method'] ?>で<br>よろしいでしょうか</p>
+                <p>開催するイベント<?= $_POST['name'] ?><br>日付<?= $_POST['date'] ?><br>スペース<br>縦<?= $_POST['sizex'] ?>横<?= $_POST['sizey'] ?><br>
+                        予約方法<br><?= $_POST['method'] ?>で<br>画像<?= $_POST['image'] ?><br>よろしいでしょうか</p>
                 <p><?= $_GET['address'] ?></p>
                 <input type="hidden" name="name" value="<?= $_POST['name'] ?>">
                 <input type="hidden" name="date" value="<?= $_POST['date'] ?>">
-                <input type="hidden" name="size" value="<?= $_POST['size'] ?>">
+                <input type="hidden" name="size" value="<?= $_POST['sizex'] ?>">
+                <input type="hidden" name="size" value="<?= $_POST['sizey'] ?>">
                 <input type="hidden" name="method" value="<?= $_POST['method'] ?>">
                 <input type="hidden" name="image" value="<?= $_POST['image'] ?>">
                 <input type="hidden" name="address" value="<?= $_GET['address'] ?>">
