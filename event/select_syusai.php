494d51be504b4458e785c3e629f6e27f27ae21e0
okada <shugo140208@softbank.ne.jp>
2022-01-14 13:56:05 +0900

    デザイン統一

--- event/select_syusai.php
+++ event/select_syusai.php
@@ -1,20 +1,22 @@
 <?php require_once("../header.php"); ?>
 <!-- Section-->
-<section class="py-5">
+<section class="container py-5">
     <form action="kakunin_syusai.php?address=<?= $_GET['address'] ?>" method="post">
         <p>開催するイベント</p>
-        <input type="text" name="name">
+        <input class="form-control" type="text" name="name">
         <p>日付</p>
-        <input type="date" name="date" id="">
+        <input class="form-control" type="date" name="date" id="">
         <p>スペース</p>
-        <p>縦</p><input type="number" name="size" id=""></br>
-        <p>横</p><input type="number" name="size" id=""></br>
+        <p>縦</p>
+        <input class="form-control" type="number" name="size" id=""></br>
+        <p>横</p>
+        <input class="form-control" type="number" name="size" id=""></br>
         <p>抽選方法</p>
         <p>予約方法をお選び下さい</p>
         <input type="radio" name="method" value="1">早い者勝ち
         <input type="radio" name="method" value="2">抽選<br><br>
         <p>画像</p>
-        <input type="text" name="image">
+        <input class="form-control" type="text" name="image">
         <input type="submit" class="btn btn-success" value="確認">
     </form>
     <form action="up.php" method="post" enctype="multipart/form-data">
