d9f562594a82e45a48b4d0f665f483a64fb717d8
okada <shugo140208@softbank.ne.jp>
2022-01-18 14:54:02 +0900

    メール送信機能

--- mail.php
+++ mail.php
@@ -1,6 +1,6 @@
 <?php
 $to = "taiga.20020110@gmail.com";
 $subject = "TEST";
-$message = "小林くさ";
+$message = "あいうえお";
 $headers = "FROM: s.okada70101@gmail.com";
 mail($to, $subject, $message, $headers);
