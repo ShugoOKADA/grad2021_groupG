9b04be5f30d01f36c0214d7ef53084eabb0648cb
okada <shugo140208@softbank.ne.jp>
2022-01-21 15:21:18 +0900

    メール送信

--- reserve/reserve_tuika.php
+++ reserve/reserve_tuika.php
@@ -16,5 +16,11 @@ $sql = "INSERT INTO reserve (name, mail, eventId, pass, number) VALUES ('" . $na
 mysqli_query($link, $sql);
 
 mysqli_close($link);
+
+// $to = "taiga.20020110@gmail.com";
+// $subject = "予約完了";
+// $message = "ご予約頂き、誠にありがとうございます。予約が完了致しましたので、お知らせ致します。";
+// $headers = "FROM: s.okada70101@gmail.com";
+// mail($to, $subject, $message, $headers);
 ?>
 <a href="./kakutei_hayaimono.php">次へ</a>
