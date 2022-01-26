7e99043f6df7ebfdee1fe6736abb2358b47476bc
yamutya <ksw2070374@stu.o-hara.ac.jp>
2022-01-26 11:09:38 +0900

    席完成

--- reserve/kakunin_hayaimono.php
+++ reserve/kakunin_hayaimono.php
@@ -1,24 +1,20 @@
-<?php require_once("../header.php");?>
+<?php require_once("../header.php"); ?>
 <!-- Section-->
-        <section class="py-5">
-           
-            <form method="post" action="reserve_tuika.php">
-            <p>座席番号A-1番でよろしいでしょうか？</p>
+<section class="py-5">
 
-            <input type="submit" class="btn btn-success" value="確定">
-            <input type="button" class="btn btn-success" onclick="history.back()" value="戻る">
-            <input type="hidden" name="eventId" value="<?=$_POST['eventId'] ?>">
-            <input type="hidden" name="name" value="<?=$_POST['name'] ?>">
-            <input type="hidden" name="mail" value="<?=$_POST['mail'] ?>">
-            <input type="hidden" name="pass" value="<?=$_POST['pass'] ?>">
-            <input type="hidden" name="number" value="1" >
-            </form>
+    <form method="post" action="reserve_tuika.php">
+        <p>座席番号<?=$_POST['number'] ?>でいいですか?</p>
 
-    
-        </section>
-       
-     <?php require_once("../footer.php");?>
+        <input type="submit" class="btn btn-success" value="確定">
+        <input type="button" class="btn btn-success" onclick="history.back()" value="戻る">
+        <input type="hidden" name="eventId" value="<?= $_POST['eventId'] ?>">
+        <input type="hidden" name="name" value="<?= $_POST['name'] ?>">
+        <input type="hidden" name="mail" value="<?= $_POST['mail'] ?>">
+        <input type="hidden" name="pass" value="<?= $_POST['pass'] ?>">
+        <input type="hidden" name="number" value="<?=$_POST['number'] ?>">
+    </form>
 
 
+</section>
 
-
+<?php require_once("../footer.php"); ?>
--- reserve/seki_hayaimono.php
+++ reserve/seki_hayaimono.php
@@ -1,80 +1,61 @@
-<?PHP
-require_once("../header.php");
-$c = filter_input(INPUT_GET, "c", FILTER_DEFAULT, FILTER_REQUIRE_ARRAY);
-if (!is_null($c)) {
-  print "選択された座席:" . implode(",", $c) . "<br>";
-}
-?>
-<!-- Section-->
-<section class="py-5">
-  <table class="btn-group btn-group-toggle" data-toggle="buttons">
-    <tr>
-      <td><input type="checkbox" name="c[]" id="cA-1" value="A-1"><input type="button" class="btn btn-success" value="A-1" class="cx"></td>
-      <td><input type="checkbox" name="c[]" id="cA-2" value="A-2"><input type="button" class="btn btn-success" value="A-2" class="cx"></td>
-      <td><input type="checkbox" name="c[]" id="cA-3" value="A-3"><input type="button" class="btn btn-success" value="A-3" class="cx"></td>
-      <td><input type="checkbox" name="c[]" id="cA-4" value="A-4"><input type="button" class="btn btn-success" value="A-4" class="cx"></td>
-      <td><input type="checkbox" name="c[]" id="cA-5" value="A-5"><input type="button" class="btn btn-success" value="A-5" class="cx"></td>
-    </tr>
-    <tr>
-      <td><input type="checkbox" name="c[]" id="cB-1" value="B-1"><input type="button" class="btn btn-success" value="B-1" class="cx"></td>
-      <td><input type="checkbox" name="c[]" id="cB-2" value="B-2"><input type="button" class="btn btn-success" value="B-2" class="cx"></td>
-      <td><input type="checkbox" name="c[]" id="cB-3" value="B-3"><input type="button" class="btn btn-success" value="B-3" class="cx"></td>
-      <td><input type="checkbox" name="c[]" id="cB-4" value="B-4"><input type="button" class="btn btn-success" value="B-4" class="cx"></td>
-      <td><input type="checkbox" name="c[]" id="cB-5" value="B-5"><input type="button" class="btn btn-success" value="B-5" class="cx"></td>
-    </tr>
-    <tr>
-      <td><input type="checkbox" name="c[]" id="cC-1" value="C-1"><input type="button" class="btn btn-success" value="C-1" class="cx"></td>
-      <td><input type="checkbox" name="c[]" id="cC-2" value="C-2"><input type="button" class="btn btn-success" value="C-2" class="cx"></td>
-      <td><input type="checkbox" name="c[]" id="cC-3" value="C-3"><input type="button" class="btn btn-success" value="C-3" class="cx"></td>
-      <td><input type="checkbox" name="c[]" id="cC-4" value="C-4"><input type="button" class="btn btn-success" value="C-4" class="cx"></td>
-      <td><input type="checkbox" name="c[]" id="cC-5" value="C-5"><input type="button" class="btn btn-success" value="C-5" class="cx"></td>
-    </tr>
-    <tr>
+<?php require_once("../header.php"); ?>
+<!DOCTYPE html>
+<html lang="ja">
 
-    </tr>
-  </table>
-  <form action="kakunin_hayaimono.php" method="post">
-    <input type="submit" value="確認">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+</head>
+
+<body>
+  <?php
+  $link = mysqli_connect("localhost", "sotsuken", "sotsukenpass", "sotsuken");
+  if ($link == null) {
+    echo "接続に失敗しました：";
+  } else {
+    echo "接続に成功しました：";
+  }
+  mysqli_set_charset($link, "utf8");
+  $result = mysqli_query($link, "SELECT * FROM event where id=" . $_GET['eventId']);
+  $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+  $tate = $row['tate'];
+  $yoko = $row['yoko'];
+  ?>
+  <form action="kakunin_hayaimono.php" method="POST">
+    <p>〇は席を選べます</p>
+    <p>✕は席を選べません</p>
+    <table class="">
+      <?php $num = 0; ?>
+      <?php for ($i = 0; $i < $tate; $i++) { ?>
+        <tr>
+          <?php for ($j = 0; $j < $yoko; $j++) { ?>
+            <?php $num++;  ?>
+            <td><label class="btn btn-success"><input type="radio"name="number" value="<?= $num ?>"><?= $num ?></label></td>
+
+          <?php } ?>
+        </tr>
+      <?php } ?>
+
+    </table>
     <input type="hidden" name="eventId" value="<?= $_GET['eventId'] ?>">
     <input type="hidden" name="name" value="<?= $_GET['name'] ?>">
     <input type="hidden" name="mail" value="<?= $_GET['mail'] ?>">
     <input type="hidden" name="pass" value="<?= $_GET['pass'] ?>">
-    <input type="hidden" name="number" value="1">
-  </form>
-
-  <script type="text/javascript">
-    window.addEventListener('DOMContentLoaded', function(e) {
-      document.querySelector('#f1').addEventListener('submit', function(e) {
-        var c = [].map.call(document.querySelectorAll('[name="c[]"]:checked'), function(x) {
-          return x.value;
-        }).join(",");
-        if (c === "") {
-          alert("座席を選択してください。");
-          e.preventDefault();
+    <p><input class="btn btn-success" type="submit" value="確定"></p>
+    <script lang="javascript">
+      function toggle(target) {
+        if (target.value == "○") {
+          target.value = "✕";
         } else {
-          if (!confirm(c + " が選択されています。よろしいですか?")) {
-            e.preventDefault();
-          }
+          target.value = "○";
         }
-      });
-      [].forEach.call(document.querySelectorAll('.cx'), function(x) {
-        x.addEventListener('click', function(e) {
-          var t = e.target;
-          var n = document.querySelector('#c' + t.value);
-          n.checked = !n.checked;
-        });
-      });
-    });
-  </script>
-  <style>
-    [name="c[]"] {
-      display: none;
-    }
+      }
+    </script>
+  </form>
+</body>
 
-    [name="c[]"]:checked+.cx {
-      background-Color: skyblue;
-    }
-  </style>
-</section>
-<!-- Footer-->
+</html>
 <?php require_once("../footer.php"); ?>
