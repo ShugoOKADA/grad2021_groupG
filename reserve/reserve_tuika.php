5fd38ffb33b7bd4e30fd4a721ba2de1433879bca
okada <shugo140208@softbank.ne.jp>
2022-01-27 10:36:05 +0900

    修正

--- reserve/reserve_tuika.php
+++ reserve/reserve_tuika.php
@@ -20,8 +20,8 @@ mysqli_close($link);
 $to = $mail;
 $subject = "予約完了";
 $message = "ご予約頂き、誠にありがとうございます。予約が完了致しましたので、お知らせ致します。\n";
-$message += "イベント番号：".$eventId."\n";
-$message += "席番号：".$number."\n";
+$message .= "イベント番号：".$eventId."\n";
+$message .= "席番号：".$number."\n";
 $headers = "FROM: s.okada70101@gmail.com";
 mail($to, $subject, $message, $headers);
 ?>
